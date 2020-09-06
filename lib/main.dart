bde4e219b671bdc7a56249d66d29315df649824f
Dracvia <inc.kusuma@gmail.com>
2020-09-06 23:24:30 +0700

    Apdet Lagi

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:covid_19/constant.dart';
 import 'package:covid_19/laporan.dart';
 import 'package:covid_19/master.dart';
 import 'package:covid_19/transaksi.dart';
-import 'package:covid_19/widgets/counter.dart';
+// import 'package:covid_19/widgets/counter.dart';
 import 'package:covid_19/widgets/my_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
